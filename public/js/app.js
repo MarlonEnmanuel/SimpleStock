c93589aca4b9e4dc94f23963e3d4e0e83a998618
Marlon Enmanuel Montalvo Flores <marlon.enmanuel.mf@gmail.com>
2016-07-16 08:13:20 -0500

    Correciones menores

--- public/js/app.js
+++ public/js/app.js
@@ -708,6 +708,66 @@ SimpleStock.Views.Producto = Backbone.View.extend({
 	
 });
 
+SimpleStock.Views.Registro = Backbone.View.extend({
+	tagName 	: $('#ssregistro').attr('data-tag'),
+	className 	: $('#ssregistro').attr('data-class'),
+	template 	: _.template($('#ssregistro').html()),
+
+	events : {
+		'submit form' : 'enviar'
+	},
+
+	initialize : function(){
+		var self = this;
+
+		app.views.main.add(this);
+
+		app.routers.registrar.on('route:entrada', function(){
+			self.tipo = 'entrada';
+			self.render();
+			app.views.main.show(self);
+			app.views.header.setTitle('Entrada');
+		});
+
+		app.routers.registrar.on('route:salida', function(){
+			self.tipo = 'salida';
+			self.render();
+			app.views.main.show(self);
+			app.views.header.setTitle('Salida');
+		});
+	},
+
+	render : function(){
+		var data = {};
+		if(this.tipo=='entrada'){
+			data.strtipo = 'Entrada';
+		}else{
+			data.strtipo = 'Salida';
+		}
+		this.$el.html(this.template(data));
+		return this.$el;
+	},
+
+	enviar : function(event){
+		event.preventDefault();
+
+		var self = this;
+		var data = this.$el.find('form').serializeObject();
+		var model = new SimpleStock.Models.Movimiento(data);
+
+		model.set('tipo', self.tipo);
+
+		model.save({}, {
+			success : function(){
+				Materialize.toast('Registro Satisfactorio', 4000);
+			},
+			error : function(x, s){
+				Materialize.toast(s.responseText);
+			},
+		});
+	}
+});
+
 SimpleStock.Views.Entrysals = Backbone.View.extend({
 	tagName 	: $('#page-entrysals').attr('data-tag'),
 	className 	: $('#page-entrysals').attr('data-class'),
@@ -893,66 +953,6 @@ SimpleStock.Views.Kardex = Backbone.View.extend({
 		Materialize.toast(this.model.get('apunte'), 5000);
 	},
 	
-});
-
-SimpleStock.Views.Registro = Backbone.View.extend({
-	tagName 	: $('#ssregistro').attr('data-tag'),
-	className 	: $('#ssregistro').attr('data-class'),
-	template 	: _.template($('#ssregistro').html()),
-
-	events : {
-		'submit form' : 'enviar'
-	},
-
-	initialize : function(){
-		var self = this;
-
-		app.views.main.add(this);
-
-		app.routers.registrar.on('route:entrada', function(){
-			self.tipo = 'entrada';
-			self.render();
-			app.views.main.show(self);
-			app.views.header.setTitle('Entrada');
-		});
-
-		app.routers.registrar.on('route:salida', function(){
-			self.tipo = 'salida';
-			self.render();
-			app.views.main.show(self);
-			app.views.header.setTitle('Salida');
-		});
-	},
-
-	render : function(){
-		var data = {};
-		if(this.tipo=='entrada'){
-			data.strtipo = 'Entrada';
-		}else{
-			data.strtipo = 'Salida';
-		}
-		this.$el.html(this.template(data));
-		return this.$el;
-	},
-
-	enviar : function(event){
-		event.preventDefault();
-
-		var self = this;
-		var data = this.$el.find('form').serializeObject();
-		var model = new SimpleStock.Models.Movimiento(data);
-
-		model.set('tipo', self.tipo);
-
-		model.save({}, {
-			success : function(){
-				Materialize.toast('Registro Satisfactorio', 4000);
-			},
-			error : function(x, s){
-				Materialize.toast(s.responseText);
-			},
-		});
-	}
 });
 
 SimpleStock.Views.EditUsuario = Backbone.View.extend({
